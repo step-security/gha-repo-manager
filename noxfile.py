e7e24f7fc143c53fffab3ead5fed1864fa0525f1
amanstep <aman@stepsecurity.io>
2025-11-11 18:48:56 +0530

    fix: reverted safety specific version install

--- noxfile.py
+++ noxfile.py
@@ -41,7 +41,7 @@ mypy_type_packages = [requirement for requirement in test_requirements if requir
 def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     requirements = session.poetry.export_requirements()
-    session.install("safety==3.2.10")
+    session.install("safety")
     # ignore https://github.com/pytest-dev/py/issues/287
     # its an irresposnbily filed CVE causing nose
     session.run("safety", "check", "--full-report", f"--file={requirements}", "--ignore=51457")
